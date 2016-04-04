70b01ed5b2c8aaaf4b3b70c257e05739e3a0dcf6
Perry Anderson <perry.anderson@gmail.com>
2016-04-04 16:26:01 -0230

    Refactor

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT ||  3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Goto market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Start Internet Company',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req,res) {
   res.send('Todo API Root');
@@ -25,16 +18,27 @@ app.get('/todos', function (req,res) {
 
 app.get('/todos/:id', function (req,res) {
 	var todoid = parseInt(req.params.id,10);
-	var matchedTodo;
-	todos.forEach( function (todo) {
-		if ( todo.id === todoid )
-			matchedTodo = todo;
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoid});
 	if ( matchedTodo ) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}
+});
+
+app.post('/todos', function (req,res) {
+	var body = _.pick(req.body,'description','completed');
+
+	if ( !_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
+	console.log('description: ' + body.description );
+	res.json(body);
+	body.id = todoNextId++;
+	todos.push(body);
 });
 
 app.listen(PORT, function () {
